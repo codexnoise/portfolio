6b47f20c2f76932b116d2541b2839e147f1d621e
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2023-12-14 21:22:09 -0500

    remove Home component

--- src/app/components/about/About.tsx
+++ src/app/components/about/About.tsx
@@ -1,5 +1,10 @@
 const About = () => {
-  return <div>About</div>
+  return (
+    <>
+      <h1>Diego F. Velesaca O.</h1>
+      <h2>Desarrollador de Software FullStack</h2>
+    </>
+  )
 }
 
 export default About
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Projects from './components/projects/Projects'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <About />
       <Projects />
       <Contact />
