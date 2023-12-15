574489a837e09b31fe2418daa433482dcbe31d5c
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2023-12-14 22:39:46 -0500

    added image to About component

--- src/app/components/about/About.tsx
+++ src/app/components/about/About.tsx
@@ -1,4 +1,6 @@
+import Image from 'next/image'
 import '../../globals.css'
+
 const About = () => {
   return (
     <>
@@ -6,14 +8,25 @@ const About = () => {
         <h1>Diego F. Velesaca O.</h1>
         <h2>Desarrollador de Software FullStack</h2>
       </section>
-      <p>
-        Apasionado por la tecnología, autodidacta, proactivo, orientado
-        alconstante aprendizaje. Capaz de resolver problemas y
-        desarrollarsoluciones creativas e innovadoras mediante pensamiento
-        crítico,buenas prácticas,código limpio y eficiente. Me destaco por mi
-        trabajoindividual, tengo una comunicación asertiva cuando se trata de
-        lainteracción con el equipo de trabajo.
-      </p>
+      <div className="Description">
+        <Image
+          src="/images/home_image.jpg"
+          alt="profile"
+          width={512}
+          height={320}
+        />
+        <p>
+          Soy un desarrollador de software FullStack con experiencia en
+          desarrollo web, aplicaciones móviles y servicios de internet. Me
+          encanta trabajar en proyectos colaborativos y me gusta aprender nuevas
+          tecnologías. Apasionado por la tecnología, autodidacta, proactivo,
+          orientado alconstante aprendizaje. Capaz de resolver problemas y
+          desarrollarsoluciones creativas e innovadoras mediante pensamiento
+          crítico,buenas prácticas,código limpio y eficiente. Me destaco por mi
+          trabajoindividual, tengo una comunicación asertiva cuando se trata de
+          lainteracción con el equipo de trabajo.
+        </p>
+      </div>
     </>
   )
 }
