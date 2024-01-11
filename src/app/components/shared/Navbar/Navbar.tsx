978e74e3483025de93b13a890e43b07bf870b10f
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2024-01-10 22:40:18 -0500

    add: home.svg icon

--- src/app/components/shared/Navbar/Navbar.tsx
+++ src/app/components/shared/Navbar/Navbar.tsx
@@ -1,8 +1,9 @@
-import Link from 'next/link'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
     <nav>
+      <Image src="/icons/home.svg" alt="profile" width={100} height={100} />
       <button>Home</button>
       <button>About</button>
       <button>Projects</button>
