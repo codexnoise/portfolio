4dbe8239b4333f11b34b9f2a5af5b735e78545c2
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2024-01-15 22:36:19 -0500

    edit: contact.svg svg image

--- src/app/components/shared/Navbar/Navbar.tsx
+++ src/app/components/shared/Navbar/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <Image src="/icons/home.svg" alt="profile" width={50} height={50} />
       <Image src="/icons/projects.svg" alt="profile" width={50} height={50} />
       <Image src="/icons/about.svg" alt="profile" width={50} height={50} />
-      <Image src="/icons/home.svg" alt="profile" width={50} height={50} />
+      <Image src="/icons/contact.svg" alt="profile" width={50} height={50} />
     </nav>
   )
 }
