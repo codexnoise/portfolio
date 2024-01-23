9735f3105205d5bf646ba26600d228a8e5758efc
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2024-01-22 22:28:35 -0500

    add: links ad styles to Navbar component

--- src/app/components/shared/Navbar/Navbar.tsx
+++ src/app/components/shared/Navbar/Navbar.tsx
@@ -1,12 +1,47 @@
+import Link from 'next/link'
 import Image from 'next/image'
+import styles from './Navbar.module.css'
 
 const Navbar = () => {
   return (
     <nav>
-      <Image src="/icons/home.svg" alt="inicio" width={37} height={37} />
-      <Image src="/icons/about.svg" alt="sobre mí" width={37} height={37} />
-      <Image src="/icons/projects.svg" alt="proyectos" width={37} height={37} />
-      <Image src="/icons/contact.svg" alt="contacto" width={37} height={37} />
+      <ul className={styles.Navbar__list}>
+        <li>
+          <Link href="#home">
+            <Image src="/icons/home.svg" alt="inicio" width={37} height={37} />
+          </Link>
+        </li>
+        <li>
+          <Link href="#about-me">
+            <Image
+              src="/icons/about.svg"
+              alt="sobre mí"
+              width={37}
+              height={37}
+            />
+          </Link>
+        </li>
+        <li>
+          <Link href="#projects">
+            <Image
+              src="/icons/projects.svg"
+              alt="proyectos"
+              width={37}
+              height={37}
+            />
+          </Link>
+        </li>
+        <li>
+          <Link href="/#contact-me">
+            <Image
+              src="/icons/contact.svg"
+              alt="contacto"
+              width={37}
+              height={37}
+            />
+          </Link>
+        </li>
+      </ul>
     </nav>
   )
 }
