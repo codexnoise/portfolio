cca9b5fad976ddd6b98ec61ba4c32cdf6d74c12e
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2024-01-23 20:42:18 -0500

    add: Technologies component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import About from './components/about/About'
 import Contact from './components/contact/Contact'
 import Projects from './components/projects/Projects'
+import Technologies from './components/technologies/Technologies'
 
 export default function Home() {
   return (
     <main>
       <About />
+      <Technologies />
       <Projects />
       <Contact />
     </main>
