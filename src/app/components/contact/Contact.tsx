98ff5c456f640187924f92477bdd52189de00b1e
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2024-01-28 21:19:08 -0500

    Refactor code for About and Contact components

--- src/app/components/contact/Contact.tsx
+++ src/app/components/contact/Contact.tsx
@@ -1,6 +1,11 @@
 const Contact = () => {
-  //console.log('sdbsdbsdb')
-  return <div>Conctact</div>
+  return (
+    <section id="contact">
+      <section className="About">
+        <h2>CONTACTO</h2>
+      </section>
+    </section>
+  )
 }
 
 export default Contact
