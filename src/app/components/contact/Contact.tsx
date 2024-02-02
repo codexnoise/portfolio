477a2d35f749d5f3afb0d5aac017dce0f57ea190
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2024-02-01 22:13:59 -0500

    Add contact section with social media icons and email link

--- src/app/components/contact/Contact.tsx
+++ src/app/components/contact/Contact.tsx
@@ -1,35 +1,39 @@
 import Image from 'next/image'
+import styles from './Contact.module.css'
 
 const Contact = () => {
   return (
     <section id="contact">
       <section className="About">
         <h2>CONTACTO</h2>
-        <p>
-          👩🏽‍💻 Te invito a que mantengamos contacto a través de mis redes
-          sociales:, ó si deseas puedes escribirme un mail.
+        <p className={styles.Contact__text}>
+          👩🏽‍💻 Te invito a que mantengamos contacto a través de mis redes sociales
+          ó si deseas puedes escribirme un mail
+          <a href="mailto:diegovo@outlook.com" className={styles.Contact__link}>
+            {' '}
+            ✉️
+          </a>
         </p>
-        <a href="mailto:diegovo@outlook.com">✉️</a>
-        <div>
+        <div className={styles.Contact__container}>
           <Image
             src="/icons/linkedin.svg"
             alt="inicio"
-            width={450}
+            width={45}
             height={45}
           />
           <Image src="/icons/github.svg" alt="inicio" width={45} height={45} />
+          <Image src="/icons/twitter.svg" alt="inicio" width={45} height={45} />
           <Image
             src="/icons/whatsapp.svg"
             alt="inicio"
             width={45}
             height={45}
           />
-          <Image src="/icons/twitter.svg" alt="inicio" width={45} height={45} />
           <Image
             src="/icons/instagram.svg"
             alt="inicio"
-            width={45}
-            height={45}
+            width={50}
+            height={50}
           />
         </div>
       </section>
