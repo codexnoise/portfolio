b132ef26a909fa2fce41f85de7b1412c3ffc2502
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2024-04-21 15:44:50 -0500

    Update image paths in Projects and Technologies components

--- src/app/components/technologies/Technologies.tsx
+++ src/app/components/technologies/Technologies.tsx
@@ -8,85 +8,85 @@ const Technologies = () => {
         <h2>TECNOLOGIAS Y HERRAMIENTAS</h2>
         <div className={styles.technologies__container}>
           <Image
-            src="portfolio/brands/typescript.svg"
+            src="/portfolio/brands/typescript.svg"
             alt="typescript_logo"
             width={45}
             height={45}
           />
           <Image
-            src="portfolio/brands/javascript.svg"
+            src="/portfolio/brands/javascript.svg"
             alt="javascript_logo"
             width={45}
             height={45}
           />
           <Image
-            src="portfolio/brands/next.svg"
+            src="/portfolio/brands/next.svg"
             alt="next_logo"
             width={45}
             height={45}
           />
           <Image
-            src="portfolio/brands/react.svg"
+            src="/portfolio/brands/react.svg"
             alt="react_logo"
             width={45}
             height={45}
           />
           <Image
-            src="portfolio/brands/html5.svg"
+            src="/portfolio/brands/html5.svg"
             alt="html5_logo"
             width={45}
             height={45}
           />
           <Image
-            src="portfolio/brands/css3.svg"
+            src="/portfolio/brands/css3.svg"
             alt="css3_logo"
             width={45}
             height={45}
           />
           <Image
-            src="portfolio/brands/node.svg"
+            src="/portfolio/brands/node.svg"
             alt="node_logo"
             width={45}
             height={45}
           />
           <Image
-            src="portfolio/brands/python.svg"
+            src="/portfolio/brands/python.svg"
             alt="python_logo"
             width={45}
             height={45}
           />
           <Image
-            src="portfolio/brands/fastapi.svg"
+            src="/portfolio/brands/fastapi.svg"
             alt="fastapi_logo"
             width={45}
             height={45}
           />
           <Image
-            src="portfolio/brands/mysql.svg"
+            src="/portfolio/brands/mysql.svg"
             alt="mysql_logo"
             width={60}
             height={60}
           />
           <Image
-            src="portfolio/brands/postgresql.svg"
+            src="/portfolio/brands/postgresql.svg"
             alt="postgresql_logo"
             width={60}
             height={60}
           />
           <Image
-            src="portfolio/brands/mongodb.svg"
+            src="/portfolio/brands/mongodb.svg"
             alt="mongodb"
             width={45}
             height={45}
           />
           <Image
-            src="portfolio/brands/git.svg"
+            src="/portfolio/brands/git.svg"
             alt="git_logo"
             width={45}
             height={45}
           />
           <Image
-            src="portfolio/brands/github.svg"
+            src="/portfolio/brands/github.svg"
             alt="github_logo"
             width={45}
             height={45}
